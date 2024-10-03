88971d0d37da0da0ac414812e1d45ef97b5ea2e1
jchas2 <jchas2@icloud.com>
2024-10-03 17:48:06 +1000

    Sysheader view + GlobalMemoryStatusEx

---
+++ src/taskmgr/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using Task.Manager.Configuration;
+using Task.Manager.System;
+using Task.Manager.System.Process;
+using Task.Manager.Views;
+
+namespace Task.Manager;
+
+public sealed class MainWindow
+{
+    private readonly SystemHeader _systemHeader;
+    private readonly IProcesses _processes;
+    private readonly ISystemTerminal _terminal;
+    private readonly Theme _theme;
+
+    public MainWindow(
+        SystemHeader systemHeader,
+        IProcesses processes,
+        ISystemTerminal terminal,
+        Theme theme)
+    {
+        _systemHeader = systemHeader;
+        _processes = processes;
+        _terminal = terminal ?? throw new ArgumentNullException(nameof(terminal));
+        _theme = theme ?? throw new ArgumentNullException(nameof(theme));
+    }
+}
