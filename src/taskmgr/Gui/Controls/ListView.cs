2ebe162ff6d45bcc25e5e22bcaf64c36732b70eb
jchas2 <jchas2@icloud.com>
2024-10-15 17:49:03 +1000

    Setup ListView Item Collection

--- src/taskmgr/Gui/Controls/ListView.cs
+++ src/taskmgr/Gui/Controls/ListView.cs
@@ -2,6 +2,24 @@
 
 public class ListView
 {
-    public ConsoleColor ForegroundColour { get; set; }
+    private readonly ListViewItemCollection _itemCollection;
+    private List<ListViewItem> _items = [];
+
+    public ListView()
+    {
+        _itemCollection = new ListViewItemCollection(this);
+    }
+    
     public ConsoleColor BackgroundColour { get; set; }
+
+    public ConsoleColor ForegroundColour { get; set; }
+
+    public void InsertItems(ListViewItem[] items)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+        
+        _items.AddRange(items);
+    }
+    
+    public ListViewItemCollection Items => _itemCollection;
 }
---
+++ src/taskmgr/Gui/Controls/ListViewItemCollection.cs
@@ -0,0 +1,65 @@
+﻿namespace Task.Manager.Gui.Controls;
+
+public sealed class ListViewItemCollection
+{
+    private readonly ListView _owner;
+    
+    public ListViewItemCollection(ListView owner)
+    {
+        _owner = owner ?? throw new ArgumentNullException(nameof(owner));
+    }
+    
+    public void Add(ListViewItem item)
+    {
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
+        
+        _owner.InsertItems([item]);
+    }
+
+    public void AddRange(params ListViewItem[] items)
+    {
+        ArgumentNullException.ThrowIfNull(items, nameof(items));
+        
+        _owner.InsertItems(items);
+    }
+
+    public void Clear()
+    {
+        
+    }
+
+    public bool Contains(ListViewItem item)
+    {
+        return false;
+    }
+
+    public void CopyTo(Array array, int index)
+    {
+        
+    }
+    
+    public int Count { get; }
+
+    // public IEnumerator GetEnumerator()
+    // {
+    // }
+
+    public int IndexOf(ListViewItem item)
+    {
+        return -1;
+    }
+
+    public void Remove(ListViewItem item)
+    {
+        
+    }
+    
+    public ListViewItem this[int index]
+    {
+        // get {
+        // }
+        set {
+            
+        }
+    }
+}
