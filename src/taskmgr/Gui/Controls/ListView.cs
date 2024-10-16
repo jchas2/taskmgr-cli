de0ac3697c598d570091ffb8d6345f4a475068cc
jchas2 <jchas2@icloud.com>
2024-10-16 17:58:02 +1000

    ListviewIemCollection -> ListView.Items updates

--- src/taskmgr/Gui/Controls/ListView.cs
+++ src/taskmgr/Gui/Controls/ListView.cs
@@ -2,7 +2,14 @@
 
 public class ListView
 {
+    /* 
+     * The _itemCollection acts as a proxy for updates to the underlying _items List.
+     * This provides a clean api for interacting with an Items collection on the ListView
+     * control, similar to the old Win32 ListView common control. 
+     */
     private readonly ListViewItemCollection _itemCollection;
+
+    /* The container holding the items for rendering. We don't expose this via a public api. */
     private List<ListViewItem> _items = [];
 
     public ListView()
@@ -12,14 +19,48 @@ public class ListView
     
     public ConsoleColor BackgroundColour { get; set; }
 
+    internal void Clear()
+    {
+        _items.Clear();
+    }
+
+    internal bool Contains(ListViewItem item)
+    {
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
+        return _items.Contains(item);
+    }
+    
     public ConsoleColor ForegroundColour { get; set; }
 
-    public void InsertItems(ListViewItem[] items)
+    internal ListViewItem GetItemByIndex(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index, nameof(index));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, _items.Count, nameof(index));
+
+        return _items[index];
+    }
+
+    internal void InsertItem(int index, ListViewItem item)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index, nameof(index));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(index, _items.Count, nameof(index));
+        
+        _items.Insert(index, item);
+    }
+    
+    internal void InsertItems(ListViewItem[] items)
     {
         ArgumentNullException.ThrowIfNull(items);
-        
         _items.AddRange(items);
     }
     
     public ListViewItemCollection Items => _itemCollection;
+
+    internal void RemoveAt(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index, nameof(index));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, _items.Count, nameof(index));
+            
+        _items.RemoveAt(index);
+    }
 }
