25bf52d02dce7a7afb0affaa254aab6bd477cd38
jchas2 <jchas2@icloud.com>
2024-12-01 13:08:41 +1000

    ConfigParser: Tests

---
+++ tests/Task.Manager.System.UnitTests/Configuration/When_Using_ConfigSection.cs
@@ -0,0 +1,34 @@
+﻿using Task.Manager.System.Configuration;
+
+namespace Task.Manager.System.UnitTests.Configuration;
+
+public class When_Using_ConfigSection
+{
+    [Fact]
+    public void Should_Add_Section_With_Strings()
+    {
+        var section = new ConfigSection("Strings")
+            .Add("Key1", "Value1")
+            .Add("Key2", "Value2")
+            .Add("Key3", "Value3");
+        
+        Assert.Equal("Value1", section.GetString("Key1"));
+        Assert.Equal("Value2", section.GetString("Key2"));
+        Assert.Equal("Value3", section.GetString("Key3"));
+    }
+
+    [Fact]
+    public void Should_Add_Section_With_Value_Types()
+    {
+        var section = new ConfigSection("Value-Types")
+            .Add("Key1", "12345678")
+            .Add("Key2", "-12345678")
+            .Add("Key3", "true")
+            .Add("Key4", "false");
+        
+        Assert.Equal(12345678, section.GetInt("Key1"));
+        Assert.Equal(-12345678, section.GetInt("Key2"));
+        Assert.True(section.GetBool("Key3"));
+        Assert.False(section.GetBool("Key4"));
+    }
+}
