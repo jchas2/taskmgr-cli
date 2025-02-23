c46724c32cfa3066bf0084aa0b9cef0cc2a530ad
jchas2 <jchas2@icloud.com>
2025-02-23 15:39:08 +1000

    Fix % stats reporting

--- src/Task.Manager.System/SystemInfo.cs
+++ src/Task.Manager.System/SystemInfo.cs
@@ -41,7 +41,7 @@ public partial class SystemInfo : ISystemInfo
         }
     }
 
-    public bool GetSystemMemory(SystemStatistics systemStatistics) => GetSystemMemoryInternal(ref systemStatistics);
+    public bool GetSystemMemory(SystemStatistics systemStatistics) => GetSystemMemoryInternal(systemStatistics);
 
     public bool GetSystemInfo(SystemStatistics systemStatistics)
     {
@@ -50,7 +50,7 @@ public partial class SystemInfo : ISystemInfo
         systemStatistics.MachineName = Environment.MachineName;
         systemStatistics.CpuCores = (ulong)Environment.ProcessorCount;
         
-        bool result = GetCpuInfoInternal(ref systemStatistics);
+        bool result = GetCpuInfoInternal(systemStatistics);
 
         var ip = GetPreferredIpAddress();
         
