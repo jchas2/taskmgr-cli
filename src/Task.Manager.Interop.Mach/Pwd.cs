05e159c6162cc9e5c4b118da6587640658e3cefc
jason chase <jason.chase@comtrac.com.au>
2025-03-04 14:13:16 +1000

    Add pwd.h defs

---
+++ src/Task.Manager.Interop.Mach/Pwd.cs
@@ -0,0 +1,22 @@
+using System.Runtime.InteropServices;
+
+namespace Task.Manager.Interop.Mach;
+
+public class Pwd
+{
+    public unsafe struct Passwd
+    {
+        public const int InitialBufferSize = 256;
+
+        public byte* Name;
+        public byte* Password;
+        public uint  UserId;
+        public uint  GroupId;
+        public byte* UserInfo;
+        public byte* HomeDirectory;
+        public byte* Shell;
+    }
+
+    [DllImport(Libraries.LibSystemNative, EntryPoint = "SystemNative_GetPwUidR", SetLastError = false)]
+    public static extern unsafe int GetPwUidR(uint uid, out Passwd pwd, byte* buf, int bufLen);
+}
