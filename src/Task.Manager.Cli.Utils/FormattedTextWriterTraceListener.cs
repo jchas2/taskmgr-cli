bc00b7506690867e8cb735dca86e42650563b580
jchas2 <jchas2@icloud.com>
2025-04-25 06:05:40 +1000

    Debug trace listening formatted to txt file

---
+++ src/Task.Manager.Cli.Utils/FormattedTextWriterTraceListener.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+
+namespace Task.Manager.Cli.Utils;
+
+public class FormattedTextWriterTraceListener(string fileName) : TextWriterTraceListener(fileName)
+{
+    public override void Write(string? message)
+    {
+        string formattedMessage = FormatMessage(message);
+        base.Write(formattedMessage);
+    }
+
+    public override void WriteLine(string? message)
+    {
+        string formattedMessage = FormatMessage(message);
+        base.WriteLine(formattedMessage);
+    }
+
+    public static void Initialise()
+    {
+        string fileName = UseNextFileName();
+        var traceListener = new FormattedTextWriterTraceListener(fileName);
+        Trace.Listeners.Add(traceListener);
+        Trace.AutoFlush = true;
+    }
+    
+    public static string UseNextFileName(string prefix = "debug", string extension = "txt")
+    {
+        /* Use a timestamp format that sorts chronologically: "yyyyMMdd_HHmmss_fff" */
+        string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff");
+        return $"{prefix}_{timestamp}.{extension}";
+    }
+
+    private string FormatMessage(string? message)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        return $"[{timestamp}] [Thread-{Thread.CurrentThread.ManagedThreadId}] {message}";
+    }
+}
