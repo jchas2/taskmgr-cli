de386a029f60eb6ee95b7b149557e84f08d85451
jchas2 <jchas2@icloud.com>
2025-10-11 11:07:43 +1000

    Fix MacOS product name test

---
+++ tests/Task.Manager.Tests/RunContextTests.cs
@@ -0,0 +1,40 @@
+using Moq;
+using Task.Manager.Cli.Utils;
+using Task.Manager.Internal.Abstractions;
+using Task.Manager.Process;
+using Task.Manager.System;
+using Task.Manager.System.Process;
+
+namespace Task.Manager.Tests;
+
+public class RunContextTests
+{
+    [Fact]
+    public void Should_Create_RunContext()
+    {
+        Mock<IFileSystem> fileSystem = new();
+        Mock<IProcessService> processService = new();
+        Mock<IModuleService> moduleService = new();
+        Mock<IThreadService> threadService = new();
+        Mock<ISystemInfo> systemInfo = new();
+        Mock<IProcessor> processor = new();
+        Mock<IOutputWriter> outputWriter = new();
+
+        RunContext context = new(
+            fileSystem.Object,
+            processService.Object,
+            moduleService.Object,
+            threadService.Object,
+            systemInfo.Object,
+            processor.Object,
+            outputWriter.Object);
+        
+        Assert.True(context.FileSystem == fileSystem.Object);
+        Assert.True(context.ProcessService == processService.Object);
+        Assert.True(context.ModuleService == moduleService.Object);
+        Assert.True(context.ThreadService == threadService.Object);
+        Assert.True(context.SystemInfo == systemInfo.Object);
+        Assert.True(context.Processor == processor.Object);
+        Assert.True(context.OutputWriter == outputWriter.Object);
+    }
+}
